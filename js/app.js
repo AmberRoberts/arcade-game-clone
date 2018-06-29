377d5040a6122dce0734e41ab58130795a94cdb9
Amber Roberts <amberrobertsvt@gmail.com>
2018-06-29 11:24:17 -0400

    Character renders on game board

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+  constructor(){
+    this.sprite = 'images/enemy-bug.png';
+    this.x = 100;
+    this.y = 90;
+  }
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Update the enemy's position, required method for game
@@ -22,14 +29,28 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+class Player {
+  constructor() {
+  this.sprite = 'images/char-horn-girl.png';
+  this.x = 200;
+  this.y = 400;
+}
+render() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+};
 // This class requires an update(), render() and
 // a handleInput() method.
+// Player.prototype.update = function(dt) {
+// }
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+let allEnemies = [];
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
